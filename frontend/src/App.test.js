623748645be06cd9395e1003a4fed8276a522553
Nor Shukri <shukri@sodrama.sg>
2024-05-25 08:04:13 +0800

    Mitigate the fetch error

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,10 +1,16 @@
+// src/App.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ results: [] }),
+  })
+);
 
 test('renders Netflix Clone header', () => {
   render(<App />);
-  const headerElement = screen.getByText(/Netflix Clone/i);
-  expect(headerElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/Netflix Clone/i);
+  expect(linkElement).toBeInTheDocument();
 });
