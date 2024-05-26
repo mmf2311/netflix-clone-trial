e2000ffd5527d369adc41f2f213e749fe79bae29
koolidude <norshukri87@gmail.com>
2024-05-26 14:06:25 +0800

    Delete backend/src directory

--- backend/src/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-def get_movie_data(title):
-    api_key = os.getenv('TMDB_API_KEY')
-    url = f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={title}'
-    response = requests.get(url)
-    return response.json()
