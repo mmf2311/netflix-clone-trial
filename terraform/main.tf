6c0d8486669c87bf47e45ca2f073b250407c7d30
Nor Shukri <shukri@sodrama.sg>
2024-05-27 04:13:02 +0800

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -138,14 +138,3 @@ resource "aws_ecs_service" "netflix_clone_service" {
   ]
 }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.netflix_clone.repository_url
-}
-
-output "cluster_name" {
-  value = aws_eks_cluster.eks_cluster.name
-}
-
-output "kubeconfig" {
-  value = aws_eks_cluster.eks_cluster.endpoint
-}
