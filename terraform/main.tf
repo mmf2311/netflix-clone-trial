9be7f4161ef6619a09e74519645d90db1f0bae34
Nor Shukri <shukri@sodrama.sg>
2024-05-27 14:29:17 +0800

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,73 +1,82 @@
 locals {
-  timestamp = replace(formatdate("YYYYMMDD-HHmmss", timestamp()), ":", "")
+  timestamp = replace(formatdate("YYYYMMDD-HHMMSS", timestamp()), ":", "")
 }
 
 resource "aws_vpc" "netflix_clone_vpc" {
   cidr_block = "10.0.0.0/16"
-
   tags = {
     Name = "group-3-vpc-netflix-clone-${var.branch_name}-${local.timestamp}"
+  }
+}
+
+resource "aws_subnet" "netflix_clone_subnet" {
+  vpc_id     = aws_vpc.netflix_clone_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "group-3-subnet-netflix-clone-${var.branch_name}-${local.timestamp}"
   }
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "group-3-ecsTaskExecutionRole-${var.branch_name}-${local.timestamp}"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
       }
-    ]
+    }]
+    Version = "2012-10-17"
   })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
 }
 
 resource "aws_ecs_cluster" "netflix_clone_cluster" {
   name = "group-3-ecs-cluster-netflix-clone-${var.branch_name}-${local.timestamp}"
-
-  tags = {
-    Name = "group-3-ecs-cluster-netflix-clone-${var.branch_name}-${local.timestamp}"
-  }
 }
 
 resource "aws_ecr_repository" "netflix_clone" {
   name = "group-3-ecr-netflix-clone-${var.branch_name}-${local.timestamp}"
+}
 
-  image_tag_mutability = "MUTABLE"
+resource "aws_ecs_task_definition" "netflix_clone_task" {
+  family                   = "group-3-ecs-task-netflix-clone-${var.branch_name}-${local.timestamp}"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = jsonencode([{
+    name  = "netflix-clone"
+    image = "${aws_ecr_repository.netflix_clone.repository_url}:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 5000
+      hostPort      = 5000
+    }]
+    environment = [{
+      name  = "TMDB_API_KEY"
+      value = var.tmdb_api_key
+    }]
+  }])
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
   name = "group-3-eks-netflix-clone-${var.branch_name}-${local.timestamp}"
   role_arn = aws_iam_role.eks_cluster_role.arn
-
   vpc_config {
-    subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+    subnet_ids = [aws_subnet.netflix_clone_subnet.id, aws_subnet.netflix_clone_subnet.id]
   }
 }
 
 resource "aws_iam_role" "eks_cluster_role" {
   name = "group-3-eks-cluster-role-${var.branch_name}-${local.timestamp}"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.eks_cluster_role.name
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
@@ -75,23 +84,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
 }
 
-resource "aws_subnet" "subnet_a" {
-  vpc_id     = aws_vpc.netflix_clone_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"  # Change this to your desired AZ
-
-  tags = {
-    Name = "group-3-subnet-a-netflix-clone-${var.branch_name}-${local.timestamp}"
-  }
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
 }
-
-resource "aws_subnet" "subnet_b" {
-  vpc_id     = aws_vpc.netflix_clone_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"  # Change this to your desired AZ
-
-  tags = {
-    Name = "group-3-subnet-b-netflix-clone-${var.branch_name}-${local.timestamp}"
-  }
-}
-
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.netflix_clone.repository_url
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
 }
 
-output "cluster_name" {
-  value = aws_ecs_cluster.netflix_clone_cluster.name
+output "ecr_repository_url" {
+  value = aws_ecr_repository.netflix_clone.repository_url
 }
 
 output "kubeconfig" {
