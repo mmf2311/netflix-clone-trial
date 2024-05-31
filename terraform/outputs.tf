2f63debff04de4bba1291dad00b34fbb37e112b2
Nor Shukri <shukri@sodrama.sg>
2024-05-31 18:29:10 +0800

    test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,13 +6,13 @@ output "kubeconfig" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
 
-output "vpc_id" {
-  value = aws_vpc.netflix_clone_vpc.id
-}
-
 output "subnet_ids" {
   value = [
     aws_subnet.netflix_clone_subnet_1.id,
     aws_subnet.netflix_clone_subnet_2.id
   ]
+}
+
+output "vpc_id" {
+  value = aws_vpc.netflix_clone_vpc.id
 }
