9fc72c630cad2fbd08ca7b4e5bf2a78746813cac
Nor Shukri <shukri@sodrama.sg>
2024-06-01 09:24:53 +0800

    test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "aws_account_id" {
+  description = "The AWS account ID."
+  type        = string
+}
+
 variable "aws_region" {
   description = "The AWS region to deploy to."
   default     = "us-east-1"
@@ -9,10 +14,5 @@ variable "branch_name" {
 
 variable "tmdb_api_key" {
   description = "The API key for TMDB."
-  type        = string
-}
-
-variable "aws_account_id" {
-  description = "The AWS account ID."
   type        = string
 }
